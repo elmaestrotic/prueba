1635f33eeba37850b5c79ed54eebbd258ea7584d
Alexander <elmaestrotic@gmail.com>
2022-02-23 11:07:47 -0500

    Ejemplo de Poo con el proyecto Automovil

---
+++ automovil/src/automovil/AutoMovil.java
@@ -0,0 +1,25 @@
+package automovil;
+
+public class AutoMovil {
+    //Atributos
+    String color, marca, modelo;
+    int motor;//número de caballos de fuerza del motor
+
+    //Métodos
+    void arrancar(){
+        System.out.println("Vehículo encendido");
+    }
+    void acelerar(){
+        System.out.println("Nos fuimos ma' niño..");
+    }
+    void girar(){
+        System.out.println("Mera vuelta pa'");
+    }
+    void frenar(){
+        System.out.println("El vehículo se ha detenido");
+    }
+    void reversar(){
+        System.out.println("Dale pa' tra..tratra..tra..");
+    }
+
+}
---
+++ automovil/src/automovil/Main.java
@@ -0,0 +1,17 @@
+package automovil;
+
+import automovil.AutoMovil;
+
+public class Main {
+    public static void main(String[] args) {
+        AutoMovil obj1 = new AutoMovil();
+        obj1.arrancar();
+        obj1.acelerar();
+        obj1.girar();
+        obj1.reversar();
+        obj1.frenar();
+    }
+
+
+
+}
